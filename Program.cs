f912d44f431c104cead9219c0f249676f657a211
Tutto <tuttoandcomeon@outlook.com>
2023-08-15 13:12:47 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.Metrics;
+
+Meter s_meter = new("HatCo.HatStore", "1.0.0");
+Counter<int> s_hatsSold = s_meter.CreateCounter<int>("hats-sold");
+
+var rand = Random.Shared;
+Console.WriteLine("Press any key to exit");
+while (!Console.KeyAvailable)
+{
+    //// Simulate hat selling transactions.
+    Thread.Sleep(rand.Next(100, 2500));
+    s_hatsSold.Add(rand.Next(0, 1000));
+}
